89e3daf744a8e37898d58d2b513e51bc0568746f
bw4sz <benweinstein2010@gmail.com>
2017-03-27 11:07:16 -0700

    pushing for ari to see in buenos aries

--- IceAnalysis.md
+++ IceAnalysis.md
@@ -2,12 +2,34 @@
 Ben Weinstein  
 February 21, 2017  
 
+# Project Description 
+  
+  * Global changes in climate will lead to new environments
+  
+  * A key question is how sensitive animals are to changes in environmental conditions
+  
+  * When favorable conditions arise, how quickly can animals respond to the availability of new habitat?
+  
+  * Polar marine ecosystems are particularly vulnerable, given the pronounced effects of climate on local conditions, variable environments, and lack of information on species distributions
+  
+  * We evaluate the response of two cetacean species to changes in sea-ice cover.
+  
+##	Our aims
+
+  * Sea ice trends, by month, over the last 5 years
+
+  * Determine the threshold of sea-ice concentration and proximity on whale presence
+  
+  * Evaluate the phenology of sea-ice timing and whale movement
+  
+  * Highlight anomalous sea ice conditions  
+  * % Change in available habitat based on variable ice conditions
 
 
-#Argos Observations
 
 
-## Occupancy and ice concentration.
+
+# Occupancy and ice concentration.
 
 What is the probability of occupancy of a cell as a function of % ice cover.
 
@@ -25,7 +47,7 @@ Associate each argos location with ice cell.
 
 Each observation is not independent. In the above analysis a whale popping up 4 times in an hour will recieve the same weight as 4 observations in one day. One approach is to take the average ice concentration among observations and multiply it be the time difference.
 
-![](IceAnalysis_files/figure-html/unnamed-chunk-7-1.png)<!-- -->![](IceAnalysis_files/figure-html/unnamed-chunk-7-2.png)<!-- -->
+![](IceAnalysis_files/figure-html/unnamed-chunk-7-1.png)<!-- -->
 
 # Null models
 
@@ -39,17 +61,17 @@ For each day, sample randomly in the background points for null values of sea ic
 
 ![](IceAnalysis_files/figure-html/unnamed-chunk-9-1.png)<!-- -->![](IceAnalysis_files/figure-html/unnamed-chunk-9-2.png)<!-- -->
 
-## Temperal window
+#Distance to ice edge
+
+![](IceAnalysis_files/figure-html/unnamed-chunk-10-1.png)<!-- -->
+
+![](IceAnalysis_files/figure-html/unnamed-chunk-11-1.png)<!-- -->![](IceAnalysis_files/figure-html/unnamed-chunk-11-2.png)<!-- -->
+
+# Temperal window
 
 Sample the cells in a sliding window
 
 * we are allowed to sample any cells that have been occupied in the dataset, we know that whales can disperse to those sites. This ignores the inhenerent spatial autocorrelation in movement.
-
-#Distance to ice edge
-
-
-
-![](IceAnalysis_files/figure-html/unnamed-chunk-11-1.png)<!-- -->![](IceAnalysis_files/figure-html/unnamed-chunk-11-2.png)<!-- -->
 
 
 
--- ice_download.R
+++ ice_download.R
@@ -4,6 +4,7 @@ library(raster)
 library(curl)
 library(rgdal)
 library(gdalUtils)
+library(dplyr)
 
 #Build registry
 baseurl<-'http://www.iup.uni-bremen.de:8084/amsr2data/asi_daygrid_swath/s3125'
@@ -21,7 +22,7 @@ mdat<-read.csv("Data/Humpback Whales Megaptera novaeangliae West Antarctic Penin
 #unique days
 #set datestamp
 mdat$timestamp<-as.POSIXct(mdat$timestamp,format="%Y-%m-%d %H:%M:%S.000")
-udays<-unique(strptime(mdat$timestamp,format="%Y-%m-%d"))
+udays<-unique(strptime(mdat$timestamp,format="%Y-%m-%d",tz="GMT"))
 
 region='AntarcticPeninsula'
 
@@ -83,4 +84,76 @@ for (x in 1:length(udays)){
   }
 
 
+## Get the 10 day interval within all ice day observations
+daysec = 60*60*24
 
+newdays<-list()
+for (x in 1:10){
+  newdays[[x]]<-udays + (x * daysec)
+}
+
+#bind and get unique set
+newdays<-unique(do.call("c",newdays))
+#for soem reason that resets the time zone
+newdays<-unique(strptime(newdays,format="%Y-%m-%d",tz="GMT"))
+
+#remove days that were already in the original download
+newdays<-newdays[!newdays %in% udays]
+
+#download those newdays
+#for each of these unique days, get the sea ice data 
+for (x in 1:length(newdays)){
+  datep<-newdays[x]
+  #split into components
+  mn<-format(datep,format="%m")
+  
+  #month abbreciation
+  mnabb<-tolower(month.abb[as.numeric(mn)])
+  
+  day<-format(datep,format="%d")
+  year<-format(datep,format="%Y")
+  fl<-paste('asi-AMSR2-s3125-',year,mn,day,".hdf",sep="")
+  
+  #look up month day format
+  full_url<-paste(baseurl,year,mnabb,region,fl,sep="/")
+  filname<-paste("C:/Users/Ben/Dropbox/Whales/AMSR2",year,mnabb,region,fl,sep="/")
+  
+  #create directory if needed
+  dir_check<-paste("C:/Users/Ben/Dropbox/Whales/AMSR2",year,mnabb,region,sep="/")
+  if(!dir.exists(dir_check)){
+    dir.create(dir_check,recursive=T)
+  }
+  
+  tryCatch(
+    {
+      curl_download(url=full_url,destfile=filname)
+      #create gdal version
+      flnc<-paste('asi-AMSR2-s3125-',year,mn,day,'.nc',sep="")
+      newfl<-paste("C:/Users/Ben/Dropbox/Whales/AMSR2",year,mnabb,region,flnc,sep="/")
+      
+      #netcdf transfer
+      gdal_translate(filname,newfl,sds=T)
+      
+      #read in 
+      s<-raster(readGDAL(newfl))
+      
+      #assign coords
+      proj4string(s) <- proj4string(r)
+      extent(s) <- extent(r)
+      
+      #assign coordinates
+      s<-flip(s,direction="y")
+      
+      fl_final<-paste('asi-AMSR2-s3125-',year,mn,day,'.grd',sep="")
+      savefile<-paste("C:/Users/Ben/Dropbox/Whales/AMSR2",year,mnabb,region,fl_final,sep="/")
+      writeRaster(s,savefile)
+      
+      #remove hdf and nc
+      file.remove(filname)
+      file.remove(newfl)
+      
+    }, error=function(e) {
+      print("no hdf found")
+      return(NA) 
+    })
+}
